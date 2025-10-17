e3846b8fd328375ed2abf2231040974b8a7d6453
ronak-gami <ronakg.itpath@gmail.com>
2025-10-17 10:08:30 +0530

    Refactor App component to use external styles and remove unused code

--- App.tsx
+++ App.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { VulkanView } from './src/VulkanView';
+import { useStyle } from './styles.app';
 
 function App(): React.JSX.Element {
+  const styles = useStyle();
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" />
-      <View style={styles.header}>
-        <Text style={styles.title}>Vulkan Rendering in React Native</Text>
-      </View>
       <VulkanView style={styles.vulkanView} />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-  header: {
-    padding: 20,
-    backgroundColor: '#1a1a1a',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-  },
-  vulkanView: {
-    flex: 1,
-  },
-});
 
 export default App;
---
+++ styles.app.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const useStyle = () => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+    },
+    vulkanView: {
+      flex: 1,
+    },
+  });
+};
